4a2c68b33ac356149a0aea96f05bbb02e49ab025
Wyatt Saint <77474938+wyattwsaint@users.noreply.github.com>
2021-09-02 16:38:31 -0400

    Update toDoList.java

--- toDoList.java
+++ toDoList.java
@@ -2,80 +2,73 @@ package sandbox;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class sandbox {
 
-	public static void main(String[] args) throws Exception {
-	
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader bchoice = new BufferedReader(new FileReader("taskList.txt"));
 		
-		System.out.println("Press \"a\" then \"Enter\" to add a task, or \"c\" then \"Enter\" to cross off a task.");
-		Scanner scanner = new Scanner(System.in);
-		String addOrCrossOff = scanner.next();
-		if (addOrCrossOff.equals("a")) {
-			taskDataEntry();
-		}
-		else if (addOrCrossOff.equals("c")) {
-			crossTasksOffTheList();
-		}
-		else {
-			System.out.println("Please press \"a\" or \"c\".");
+		while (bchoice.readLine() != null) {
+			System.out.println("Press e to enter a task, press q to quit/print list, or press d to delete a task");
+			Scanner scanner = new Scanner(System.in);
+			String choice = scanner.nextLine();
+			if (choice.equals("e")) {
+				addTaskAndStore();
 			}
-	
+			else if (choice.equals("q")) {
+				printTodoListToTerminal();
+				break;
+			}
+			else if (choice.equals("d")) {
+				deleteTask();
+			}
+		}
 	}
 
-	public static void taskDataEntry() throws Exception {
-		System.out.println("Enter task to accomplish: ");
+	public static void addTaskAndStore() throws IOException {
+		System.out.println("Enter task: ");
 		Scanner scanner = new Scanner(System.in);
-		String enteredTask = scanner.nextLine();
-		
-		
+		String task = scanner.nextLine();
 		BufferedWriter bw = new BufferedWriter(new FileWriter("taskList.txt", true));
-		bw.write(enteredTask);
+		bw.write(task);
 		bw.newLine();
 		bw.close();
-		
-		File myFile = new File("taskList.txt");
-		Scanner readFile = new Scanner(myFile);
-		int lineNumber = 1;
-		while (readFile.nextLine() != null) {
-			String data = readFile.nextLine();
-			System.out.println(lineNumber + ". " + data);
-			lineNumber++;
-		}
+		printTodoListToTerminal();
 	}
 
-	public static void crossTasksOffTheList() throws Exception {
-		System.out.println("Enter task to cross off: ");
+	public static void printTodoListToTerminal() throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("taskList.txt"));
+		String st = br.readLine();
+		while (st != null) {
+			System.out.println(st);
+			st = br.readLine();
+		}
+		br.close();
+	}
+
+	public static void deleteTask() throws IOException {
+		printTodoListToTerminal();
+		BufferedReader br = new BufferedReader(new FileReader("taskList.txt"));
+		BufferedWriter bw = new BufferedWriter(new FileWriter("temp.txt"));
+		System.out.println("Enter task to delete: ");
 		Scanner scanner = new Scanner(System.in);
-		String taskToCrossOff = scanner.next();
+		String deletedTask = scanner.nextLine();
 		
-		File myFile = new File("taskList.txt");
-		Scanner readFile = new Scanner(myFile);
-		int lineNumber = 1;
-		while (readFile.hasNext()) {
-			String data = readFile.nextLine();
-			System.out.println(lineNumber + ". " + data);
-			lineNumber++;
+		while (br.readLine() != null) {
+			if (deletedTask != br.readLine()) {
+				bw.write(br.readLine());
+			}
+			else if (deletedTask == br.readLine()) {
+				bw.newLine();
+			}
+			
 		}
-	}
+		
 
-	public static void addOrCrossOffTasksDecision() throws Exception {
-		System.out.println("Press \"A\" to add a task, and \"C\" to cross off a task.");
-		Scanner scanner = new Scanner(System.in);
-		String addOrCrossOff = scanner.next();
-		if (addOrCrossOff == "a") {
-			taskDataEntry();
-		} else if (addOrCrossOff == "c") {
-			crossTasksOffTheList();
-		} else {
-			System.out.println("Please press \"a\" or \"c\".");
-		}
 	}
-
 }
