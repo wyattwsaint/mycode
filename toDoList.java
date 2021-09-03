f75934d0f6772ca871ef51887c8d628a9eda22ae
Wyatt Saint <77474938+wyattwsaint@users.noreply.github.com>
2021-09-03 11:31:38 -0400

    Update toDoList.java
    To Do List complete!

--- toDoList.java
+++ toDoList.java
@@ -2,9 +2,13 @@ package sandbox;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Scanner;
 
 public class sandbox {
@@ -12,19 +16,17 @@ public class sandbox {
 	public static void main(String[] args) throws IOException {
 
 		BufferedReader bchoice = new BufferedReader(new FileReader("taskList.txt"));
-		
+
 		while (bchoice.readLine() != null) {
 			System.out.println("Press e to enter a task, press q to quit/print list, or press d to delete a task");
 			Scanner scanner = new Scanner(System.in);
 			String choice = scanner.nextLine();
 			if (choice.equals("e")) {
 				addTaskAndStore();
-			}
-			else if (choice.equals("q")) {
+			} else if (choice.equals("q")) {
 				printTodoListToTerminal();
 				break;
-			}
-			else if (choice.equals("d")) {
+			} else if (choice.equals("d")) {
 				deleteTask();
 			}
 		}
@@ -58,17 +60,28 @@ public class sandbox {
 		System.out.println("Enter task to delete: ");
 		Scanner scanner = new Scanner(System.in);
 		String deletedTask = scanner.nextLine();
-		
-		while (br.readLine() != null) {
-			if (deletedTask != br.readLine()) {
-				bw.write(br.readLine());
-			}
-			else if (deletedTask == br.readLine()) {
-				bw.newLine();
-			}
-			
-		}
-		
 
+		String line;
+		while ((line = br.readLine()) != null) {
+			if (!line.equals(deletedTask)) {
+				bw.write(line);
+				bw.newLine();
+				bw.flush();
+			} else if (line.equals(deletedTask)) {
+				System.out.println("Deleted " + line);
+			}
+		}
+		InputStream is = null;
+		OutputStream os = null;
+		is = new FileInputStream("temp.txt");
+		os = new FileOutputStream("taskList.txt");
+		byte[] tempToTaskList = new byte[1024];
+		int length;
+		while ((length = is.read(tempToTaskList)) > 0) {
+			os.write(tempToTaskList, 0, length);
+		}
+		is.close();
+		os.close();
 	}
+
 }
