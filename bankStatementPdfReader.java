062c5bc7e35b839d59433ed2cfe3608926f8e1b2
Wyatt Saint <77474938+wyattwsaint@users.noreply.github.com>
2021-10-03 22:55:44 -0400

    Update bankStatementPdfReader.java
    Latest code changes

--- bankStatementPdfReader.java
+++ bankStatementPdfReader.java
@@ -9,7 +9,7 @@ import java.io.LineNumberReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.concurrent.TimeUnit;
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,7 +18,7 @@ import org.apache.pdfbox.text.PDFTextStripper;
 
 public class lineParser {
 
-	public static void lineParseMethod() throws IOException, InterruptedException {
+	public static void lineParseMethod() throws IOException {
 
 		File file = new File("C:\\Users\\wmsai\\Desktop\\BankStatement.pdf");
 		PDFTextStripper stripper = new PDFTextStripper();
@@ -28,24 +28,114 @@ public class lineParser {
 		bw.write(pdfText);
 		bw.flush();
 		bw.close();
-		TimeUnit.SECONDS.sleep(3);
 
+		BufferedWriter clearData = new BufferedWriter(new FileWriter("csvFile"));
 		LineNumberReader lineNum = new LineNumberReader(new FileReader("bankData"));
+		String category = "MISC";
+		String[] billNames = { "PRIME VIDEO", "ROYAL FARMS", "SUNOCO", "WAWA", "ARMY", "DUNKIN", "AMAZON", "FREEDOM",
+				"CAPITAL REGION", "SHEETZ", "LOWE'S", "HOME DEPOT", "7-ELEVEN", "PROGRESS", "ONCE UPON A CHILD", "GIANT", 
+				"INSTACART", "ALDI", "COSTCO", "AAFES", "TRS TRR" };
 		String line;
 		while ((line = lineNum.readLine()) != null) {
+			BufferedWriter csvFile = new BufferedWriter(new FileWriter("csvFile", true));
 			int lineNumber = lineNum.getLineNumber();
-			String data = Files.readAllLines(Paths.get("bankData")).get(lineNumber);
-			String input = data;
-			String regex = "(\\D\\d{2}\\s-?\\d+\\D\\d{2})";
-			Matcher m = Pattern.compile(regex).matcher(input);
-			if (m.find()) {
-				System.out.println(data);
-			} else if (!m.find()) {
-				continue;
-			} else {
-				break;
+			String data = Files.readAllLines(Paths.get("bankData")).get(lineNumber - 1);
+			boolean data2 = Arrays.stream(billNames).anyMatch(data::contains);
+			if (data2 == true) {
+				String input = line;
+				String regex = "(\\D\\d{2}\\s-?\\d+\\D\\d{2})";
+				Matcher m = Pattern.compile(regex).matcher(input);
+				if (m.find()) {
+					int newLineNumber = lineNum.getLineNumber();
+					String transNamePlusPrice = Files.readAllLines(Paths.get("bankData")).get(newLineNumber - 1);
+					BufferedWriter priceTemp = new BufferedWriter(new FileWriter("priceTemp"));
+					
+					String input1 = line;
+					String regex1 = "(\\d\\d\\D\\d\\d\\D\\d\\d\\d\\d)";
+					String regex2 = "\\w\\w\\w/\\w\\w\\w\\w";
+					Matcher m1 = Pattern.compile(regex1).matcher(input1);
+					Matcher m2 = Pattern.compile(regex2).matcher(input1);
+					if (m1.find()) {
+						String[] trans10 = transNamePlusPrice.split("/", 4);
+						String trans11 = trans10[0];
+						String trans12 = String.format("%-300s", trans11);
+						String trans13 = trans12.substring(0,  40);
+						String trans14 = trans10[3].substring(0);
+						
+						csvFile.write(category + "," + trans13 + "," + trans14);
+						csvFile.newLine();
+						csvFile.flush();
+						priceTemp.write(transNamePlusPrice);
+						priceTemp.newLine();
+						priceTemp.flush();
+						continue;
+					}
+					else if (m2.find()) {
+						String[] tran = transNamePlusPrice.split("/", 3);
+						String tran1 = tran[0];
+						String tran2 = String.format("%-300s", tran1);
+						String tran3 = tran2.substring(0,  40);
+						String tran4 = tran[2].substring(0);
+						
+						csvFile.write(category + "," + tran3 + "," + tran4);
+						csvFile.newLine();
+						csvFile.flush();
+						priceTemp.write(transNamePlusPrice);
+						priceTemp.newLine();
+						priceTemp.flush();
+						continue;
+					}
+					else if (!m1.find()) {
+						String[] trans1 = transNamePlusPrice.split("/", 2);
+						String trans2 = trans1[0];
+						String trans3 = String.format("%-300s", trans2);
+						String trans4 = trans3.substring(0, 40);
+
+						String trans5 = trans1[1].substring(0);
+
+						csvFile.write(category + "," + trans4 + "," + trans5);
+						csvFile.newLine();
+						csvFile.flush();
+						priceTemp.write(transNamePlusPrice);
+						priceTemp.newLine();
+						priceTemp.flush();
+						continue;
+					}
+					
+				} else if (!m.find()) {
+					while ((line = lineNum.readLine()) != null) {
+						String input2 = line;
+						String regex2 = "(\\D\\d{2}\\s-?\\d+\\D\\d{2})";
+						Matcher m2 = Pattern.compile(regex2).matcher(input2);
+						if (m2.find()) {
+							int newLineNumber = lineNum.getLineNumber();
+							String priceLine = Files.readAllLines(Paths.get("bankData")).get(newLineNumber - 1);
+							BufferedWriter priceTemp = new BufferedWriter(new FileWriter("priceTemp"));
+
+							String data22 = String.format("%-300s", data);
+							String data33 = data22.substring(0, 40);
+							String[] data44 = priceLine.split("/", 2);
+							String data55 = data44[1].substring(0);
+
+							priceTemp.write(priceLine);
+							priceTemp.newLine();
+							priceTemp.flush();
+							csvFile.write(category + "," + data33 + "," + data55);
+							csvFile.newLine();
+							csvFile.flush();
+							break;
+						} else if (!m2.find()) {
+							continue;
+						}
+					}
+
+				} else if (data2 == false) {
+					continue;
+				}
+
 			}
 
 		}
+		System.out.println("Success");
 	}
 }
